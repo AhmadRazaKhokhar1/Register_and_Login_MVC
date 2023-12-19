131a3ca181bb2c0e3aadeb9c76386e5813bc9ffc
Ahmad Raza Khokhar <ahmadrazawebexpert@gmail.com>
2023-12-20 04:42:19 +0500

    update

--- backend/src/Middlewares/multerMiddleWare.js
+++ backend/src/Middlewares/multerMiddleWare.js
@@ -1,28 +1,28 @@
-const multer = require('multer')
-const fs = require('fs');
+import multer from 'multer';
+import fs from 'fs';
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        const uploadPath = 'C:/uploads'
-
-        cb(null, uploadPath);
-
-        fs.mkdirSync(uploadPath, { recursive: true }); // Create directory if it doesn't exist
-        cb(null, uploadPath);
-
-      },
-      filename: function (req, file, cb) {
-        cb(null, file.originalname);
-      },
+  destination:(req, file, cb)=>{
+    const uploadPath = '../../Public/images';
+    cb(null, uploadPath);
+    //if the folder does'nt exist then make it
+    fs.mkdirSync(uploadPath, {recursive:true});
+    cb(null, uploadPath);
+  },
+  filename:(req, file, cb)=>{
+    cb(null, file.originalname);
+  }
 });
 
 const upload = multer({
-    storage:storage,
-     limits:{fileSize: 10*1024*1024}  //10MB file size
-    });
-``
-const uploadSingle = upload.single('file');
-const uploadArray = upload.array('file');
-const uploadAny = upload.any('file');
+  storage:storage,
+  limits:{
+    fileSize:100*1024*1024
+  }
+});
 
-module.exports = {uploadSingle, uploadAny, uploadArray};
+const uploadFields = upload.fields(
+  [{name:"profileImage", maxCount:1},{name:"coverImage", maxCount:1}]
+  )
+
+export {uploadFields};
